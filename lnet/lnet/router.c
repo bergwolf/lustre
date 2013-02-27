6efcb109324662b7d97e768c4fefb93445786432
Peng Tao <tao.peng@emc.com>
2013-02-27 18:30:37 +0800

    LU-2850 kernel: 3.8 upstream kills daemonize()
    kernel_thread() is a low level kernel function.
    Instead of calling kernel_thread() and relying on daemonize()
    to create new kernel threads, we should really switch to
    kthread_run() and elimilating daemonize().
    
    The difference between kernel_thread() and kthread_run() is that
    the latter creates new threads from kthreadd context rather than
    as kernel_thread() from possibly user context that requires
    daemonize().
    
    Signed-off-by: Peng Tao <tao.peng@emc.com>
    Change-Id: I9f977bfc5d58192957d8c8d634204f4910edfa2a

--- lnet/lnet/router.c
+++ lnet/lnet/router.c
@@ -1095,7 +1095,8 @@ lnet_router_checker_start(void)
 
         the_lnet.ln_rc_state = LNET_RC_STATE_RUNNING;
 #ifdef __KERNEL__
-        rc = cfs_create_thread(lnet_router_checker, NULL, 0);
+	rc = PTR_ERR(cfs_kthread_run(lnet_router_checker,
+				     NULL, "router_checker"));
         if (rc < 0) {
                 CERROR("Can't start router checker thread: %d\n", rc);
                 /* block until event callback signals exit */
@@ -1230,7 +1231,6 @@ lnet_router_checker(void *arg)
         lnet_peer_t       *rtr;
         cfs_list_t        *entry;
 
-        cfs_daemonize("router_checker");
         cfs_block_allsigs();
 
         LASSERT (the_lnet.ln_rc_state == LNET_RC_STATE_RUNNING);
